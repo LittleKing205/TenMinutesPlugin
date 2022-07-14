b68d805c854b834b815d8da1133b4d280d69f811
Pascal Schreiber <pascal.schreiber.94@gmx.de>
2022-07-14 19:32:37 +0200

    Added Bot Server Leave Listener

--- src/main/java/de/pascalschreiber/tenminutesplugin/discord/listener/MemberLeaveListener.java
+++ src/main/java/de/pascalschreiber/tenminutesplugin/discord/listener/MemberLeaveListener.java
@@ -1,6 +1,8 @@
 package de.pascalschreiber.tenminutesplugin.discord.listener;
 
 import de.pascalschreiber.tenminutesplugin.discord.BotMain;
+import de.pascalschreiber.tenminutesplugin.util.Config;
+import org.bukkit.configuration.ConfigurationSection;
 import org.javacord.api.event.server.member.ServerMemberLeaveEvent;
 import org.javacord.api.listener.server.member.ServerMemberLeaveListener;
 
@@ -14,6 +16,13 @@ public class MemberLeaveListener implements ServerMemberLeaveListener {
 
     @Override
     public void onServerMemberLeave(ServerMemberLeaveEvent event) {
-        
+        Config playerListConfig = bot.getPlugin().playersListConfig;
+        ConfigurationSection playersSection = playerListConfig.getConfig().getConfigurationSection("players");
+        for (String key : playersSection.getKeys(false) ) {
+            if (playerListConfig.getConfig().getLong("players." + key + ".DiscordId") == event.getUser().getId()) {
+                playerListConfig.getConfig().set("players." + key, null);
+                playerListConfig.save();
+            }
+        }
     }
 }
